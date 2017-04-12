841d62f2fbb80a318042de60f57de8dd93858e17
81516741 <mac@mac-ld.local>
2017-04-12 15:25:06 +0800

    Initial Commit

---
+++ HRSqliteTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HRSqliteTool
+//
+//  Created by Mac on 2017/4/12.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
